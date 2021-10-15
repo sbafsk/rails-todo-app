7c0fb72afd76240dc6840f51a7b873e2162cfb53
sbafsk <razor1836@gmail.com>
2021-10-15 12:32:46 -0300

    todo app first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+     @task = Task.all # ActiveRecord relation <list>
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+  # whitelist parameters
+  def task_params
+    params.require(:task).permit(:title, :detail)
+  end
+end
