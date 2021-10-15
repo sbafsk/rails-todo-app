d1aa724e081ae03473eee09dca9c4fd8c4139bc2
sbafsk <razor1836@gmail.com>
2021-10-15 17:21:48 -0300

    +add boolean field to task schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_143916) do
+ActiveRecord::Schema.define(version: 2021_10_15_201508) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
