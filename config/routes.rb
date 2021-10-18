4e8e4fbe3d7467bf62472b28f849015f4252199f
sbafsk <razor1836@gmail.com>
2021-10-18 14:09:40 -0300

    rails g scaffold for User, bcrypt validation for password

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
+  resources :users
+  
   get 'welcome/index'
   root 'welcome#index'
 
+  resources :tasks
   get 'tasks/:id/complete/:completed', to: 'tasks#complete'
   
-  resources :tasks
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(username: "Peter", password: "elephant")
+
 Task.create(title: "Drop it like it's hot",
   detail: "whatever...")
 
