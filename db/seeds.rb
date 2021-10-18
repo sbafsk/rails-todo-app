9c06c74a79971eeb85840410528b43a658ed2207
sbafsk <razor1836@gmail.com>
2021-10-18 13:41:18 -0300

    added db seeds,saw rails db:seed and db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Drop it like it's hot",
+  detail: "whatever...")
+
+Task.create([{title: "Drop it like it's hot 1",
+detail: "whatever..."}, {title: "Drop it like it's hot 2",
+  detail: "whatever..."}])
